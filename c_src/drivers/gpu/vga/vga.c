d5eae7ecfda6eda1aa747bde02881fbebbbebec3
Renan2010 <renanluscad@gmail.com>
2025-02-15 18:29:11 -0300

    Add c_src(C language) to RenuxOS and vga driver

---
+++ c_src/drivers/gpu/vga/vga.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../../include/vga.h"
+
+void init_vga_driver() {
+    printf("Initalizing vga driver");
+}
---
+++ c_src/drivers/include/vga.h
@@ -0,0 +1,17 @@
+#ifndef VGA_H
+#define VGA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_vga_driver();
+
+
+
+}
+#ifndef __cplusplus
+
+#endif
+
+#endif // VGA_H
