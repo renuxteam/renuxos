d095820e89cb6d33bcd8d969e374f0c796d61770
Renan2010 <renanluscad@gmail.com>
2025-03-07 18:54:47 -0300

    Add init system to renux

---
+++ kernel/src/init_system/init.rs
@@ -0,0 +1,11 @@
+use crate::wrappers::vga::{clear_screen, print_text};
+
+pub fn start()
+{
+    
+    clear_screen();
+    print_text("Initalizing the system"); // Initialize the system
+
+    // write another debug mensage  
+}
+
