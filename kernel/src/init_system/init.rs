195503ed3b1a2cb55b6e6085b74327284e59734d
Renan2010 <renanluscad@gmail.com>
2025-03-08 18:25:35 -0300

    Remove hybrid kernel

--- kernel/src/init_system/init.rs
+++
@@ -1,9 +0,0 @@
-use crate::wrappers::vga::{clear_screen, print_text};
-
-pub fn start()
-{
-  clear_screen();
-  print_text("Initalizing the system"); // Initialize the system
-
-  // write another debug mensage
-}
